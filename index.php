613b12f70916e7b2c37873491989d982f88b2a4f
Lee <riseray@hotmail.com>
2015-06-12 11:32:34 -0400

    modifyed original

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
     echo "this is a demo";
+    echo "this a change on the original";
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
