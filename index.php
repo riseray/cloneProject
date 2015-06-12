8fb9ab3556244a1435262d45c0ac195b0b782cc7
Lee <riseray@hotmail.com>
2015-06-12 11:41:16 -0400

    bug fix v1.1 for production

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
     echo "this is a demo";
+    echo "add new content in r1"
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
